dd8050447e33e1fc8d94c2332f66173f1beca4a8
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-07-06 16:37:43 +0100

    test: Change assert method @test #13

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -106,7 +106,7 @@ suite('Unit Tests', function () {
     // #13
     test('#isString, #isNotString', function () {
       assert.isNotString(Math.sin(Math.PI / 4), 'A float is not a string');
-      assert.isString(process.env.PATH, 'An env variable is a string (or undefined)');
+      assert.isNotString(process.env.PATH, 'An env variable is a string (or undefined)');
       assert.isString(JSON.stringify({ type: 'object' }), 'JSON is a string');
     });
     // #14
